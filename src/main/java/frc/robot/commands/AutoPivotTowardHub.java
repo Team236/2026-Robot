2d8ea4896caddab92408e5706d401ae39436a692
Owen Garabedian <Owengarabedian@gmail.com>
2026-01-14 00:32:38 -0500

    refactored the logic for the if statement, and remove any unnessisary variables.

--- src/main/java/frc/robot/commands/AutoPivotTowardHub.java
+++ src/main/java/frc/robot/commands/AutoPivotTowardHub.java
@@ -1,8 +1,10 @@
 package frc.robot.commands;
-//https://activities.graspablemath.com/whiteboards/_b78955ecb5363c72
+
 import frc.robot.Constants;
 import frc.robot.LimelightHelpers;
 import frc.robot.subsystems.Swerve;
+
+import java.util.Optional;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 import edu.wpi.first.math.MathUtil;
@@ -10,6 +12,8 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class AutoPivotTowardHub extends Command {
@@ -21,19 +25,19 @@ public class AutoPivotTowardHub extends Command {
   private PIDController pidController;
   private Pose2d robotFieldPose;
   private double tv;
-  private int targetId;
   private double yawError;
   private double newRotation;
   private double robotX;
   private double robotY;
-  private double tagX;
-  private double tagY;
+  private double robotYaw;
   private double HUBX;
   private double HUBY;
+  private Optional<Alliance> alliance = DriverStation.getAlliance();
 
   public AutoPivotTowardHub(Swerve s_Swerve, DoubleSupplier translationSup, DoubleSupplier strafeSup, DoubleSupplier rotationSup, BooleanSupplier robotCentricSup) {
     this.s_Swerve = s_Swerve;
     addRequirements(s_Swerve);
+    
 
     this.translationSup = translationSup;
     this.strafeSup = strafeSup;
@@ -61,82 +65,51 @@ public class AutoPivotTowardHub extends Command {
       double rotationVal = MathUtil.applyDeadband(rotationSup.getAsDouble(), Constants.stickDeadband);
 
       tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0); //if target is seen
-      targetId = (int) NetworkTableInstance.getDefault().getTable("limelight").getEntry("tid").getDouble(0); //target id
 
-    // needs to be chanted for red/blue logic
-    if (tv == 1 && targetId >= 2 && targetId <=11 ) {
+    if (tv == 1) {
 
-      // inside the if statement to reduce stress if not seeing tag
+      // math concepts at https://tinyurl.com/mvjft42z
       robotFieldPose = LimelightHelpers.getBotPose2d_wpiBlue("limelight");
 
-      // gets angle of robot and gets x,y
-      double robotYaw = robotFieldPose.getRotation().getRadians();
+      // needs to be in radians for math.atan2()
+      robotYaw = robotFieldPose.getRotation().getRadians();
       robotX = robotFieldPose.getX();
       robotY = robotFieldPose.getY();
 
-      Pose2d tagPose = Constants.Targeting.ID_TO_POSE.get(targetId);
+      // alliance hub selection
+      // this needs to be reviewd to make sure code is aceptable and intergratable
+      if (alliance.isPresent() && alliance.get() == Alliance.Red) {
+        HUBX = Constants.Targeting.RED_ALLIANCE_HUB_CENTER_X;
+        HUBY = Constants.Targeting.RED_ALLIANCE_HUB_CENTER_Y;
+      } else {
+        HUBX = Constants.Targeting.BLUE_ALLIANCE_HUB_CENTER_X;
+        HUBY = Constants.Targeting.BLUE_ALLIANCE_HUB_CENTER_Y;
+      }
 
-      double hubCenterX = Constants.Targeting.RED_ALLIANCE_HUB_CENTER_X;
-      double hubCenterY = Constants.Targeting.RED_ALLIANCE_HUB_CENTER_Y;
- 
-      double angle2 = Constants.Targeting.ID_TO_POSE
-        // gets the position for the target it sees
-        .get(targetId)
-        .getRotation()
-        .getRadians();
-
-        double dx = hubCenterX - robotX;
-        double dy = hubCenterY - robotY;
-
-        double desiredYaw = Math.atan2(dy, dx);
-        yawError = MathUtil.angleModulus(desiredYaw - robotYaw);
-
-        newRotation = pidController.calculate(yawError);
-
-    } else if (tv == 1 && targetId >= 18 && targetId <=27) {
-      // inside the if statement to reduce stress if not seeing tag
-      robotFieldPose = LimelightHelpers.getBotPose2d_wpiBlue("limelight");
-
-      // gets angle of robot and gets x,y
-      double robotYaw = robotFieldPose.getRotation().getRadians();
-      robotX = robotFieldPose.getX();
-      robotY = robotFieldPose.getY();
-
-      Pose2d tagPose = Constants.Targeting.ID_TO_POSE.get(targetId);
-
-      double hubCenterX = Constants.Targeting.BLUE_ALLIANCE_HUB_CENTER_X;
-      double hubCenterY = Constants.Targeting.BLUE_ALLIANCE_HUB_CENTER_Y;
-
-      double angle2 = Constants.Targeting.ID_TO_POSE
-        // gets the position for the target it sees
-        .get(targetId)
-        .getRotation()
-        .getRadians();
-
-        double dx = hubCenterX - robotX;
-        double dy = hubCenterY - robotY;
-
+      double dx = HUBX - robotX;
+      double dy = HUBY - robotY;
 
       double desiredYaw = Math.atan2(dy, dx);
       yawError = MathUtil.angleModulus(desiredYaw - robotYaw);
 
-        newRotation = pidController.calculate(yawError);
+      newRotation = pidController.calculate(yawError);
+
     } else {
-      // incase it can't see the apriltag, let driver turn normally
       pidController.reset();
       newRotation = rotationVal;
     }
 
-    // passing in the values for drive
     s_Swerve.drive(
-            new Translation2d(translationVal, strafeVal).times(Constants.Swerve.maxSpeed), 
-            newRotation * Constants.Swerve.maxAngularVelocity, 
-            !robotCentricSup.getAsBoolean(), 
-            true
-        );
+        new Translation2d(translationVal, strafeVal)
+            .times(Constants.Swerve.maxSpeed),
+        newRotation * Constants.Swerve.maxAngularVelocity,
+        !robotCentricSup.getAsBoolean(),
+        true
+    );
   }
+
   @Override
   public boolean isFinished() {
-    return false; // runs while trigger is held
+    return false;
   }
 }
