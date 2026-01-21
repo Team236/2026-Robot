3c212ea738cb1d5bbf1b9e8456d90f4ccae7fb92
Ellen DiCarlo <edicarlo@sbcglobal.net>
2026-01-21 08:23:11 -0500

    updated isFullyRetracted and isFullyExtended

--- src/main/java/frc/robot/subsystems/BinRelease.java
+++ src/main/java/frc/robot/subsystems/BinRelease.java
@@ -12,14 +12,17 @@ import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class BinRelease extends SubsystemBase {
     
     private TalonFX motor;
     private TalonFXConfiguration motorConfig;
 
-    private DigitalInput maxRetractionDI;
-    private DigitalInput maxExtensionDI;
+    private DigitalInput maxRetractLimit; //limit switch
+    private DigitalInput maxExtendLimit;  //limit switch
+    private boolean isBinRetException;
+    private boolean isBinExtException;
 
     private PIDController pidController;
 
@@ -31,16 +34,33 @@ public class BinRelease extends SubsystemBase {
         motorConfig = new TalonFXConfiguration();
         motorConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive; //tbd
         motorConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
-        motorConfig.CurrentLimits.SupplyCurrentLimit = 0; //tbd
+        motorConfig.CurrentLimits.SupplyCurrentLimit = Constants.MotorControllers.SMART_CURRENT_LIMIT; //tbd
 
         motor = new TalonFX(0); //tbd
         motor.getConfigurator().apply(motorConfig);
 
         pidController = new PIDController(0, 0, 0); //tbd
 
-        maxRetractionDI = new DigitalInput(0); //tbd
-        maxExtensionDI = new DigitalInput(0); //tbd
+    try {
+      //  This tries to make a new digital input, and if it fails, throws an error 
+       maxRetractLimit = new DigitalInput(Constants.BinRelease.DIO_RET_LIMIT); //tbd
+    } catch (Exception e) {
+       isBinRetException = true;
+      SmartDashboard.putBoolean("exception thrown for Bin Retract limit: ", isBinRetException);
     }
+ 
+   try {
+      //  This tries to make a new digital input, and if it fails, throws an error 
+       maxExtendLimit = new DigitalInput(Constants.BinRelease.DIO_EXT_LIMIT); //tbd
+    } catch (Exception e) {
+       isBinRetException = true;
+      SmartDashboard.putBoolean("exception thrown for Bin Extend limit: ", isBinRetException);
+    }
+
+    }
+
+
+//METHODS START HERE:
 
     public void resetEncoder()
     {
@@ -53,30 +73,28 @@ public class BinRelease extends SubsystemBase {
     }
 
     public void stopMotor()
-    {
-        desiredSpeed = 0;
+    {  // desired speed is zero
         motor.set(0);
     }
 
     public double getMotorSpeed()
-    {
+    {   //returns speed between -1 and +1
         return motor.get();
     }
 
     private void setMotorSpeed(double speed)
-    {
-        desiredSpeed = speed;
+    {  //sets the motor speed to "speed" which is passed in, between -1 and +1
         motor.set(speed);
     }
 
     public boolean isFullyRetracted()
-    {
-        return maxRetractionDI.get() || getEncoderRevolutions() < 0;
+    {   //want to zero the encoder when this limit is hit
+        return maxRetractLimit.get();
     }
 
     public boolean isFullyExtended()
-    {
-        return maxExtensionDI.get() || getEncoderRevolutions() > 0;
+    {   //TBD  make sure encoder reading is increasing as mechanism extends, so the ">" sign works below
+        return (maxExtendLimit.get() || getEncoderRevolutions() > Constants.BinRelease.ENC_REVS_MAX); //set to a high value at first, for code testing
     }
 
     public void setMotorSpeedSafe(double speed)
