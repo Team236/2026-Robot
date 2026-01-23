0b08e23a4146762a86c8f2c5b318c9a2b5a1128a
AnonymousRedCat <aiuser3999@gmail.com>
2026-01-22 20:14:51 -0500

    updated again to remove instances of algaepivot

--- src/main/java/frc/robot/commands/shooterManualPivot.java
+++ src/main/java/frc/robot/commands/shooterManualPivot.java
@@ -2,10 +2,10 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.AlgaePivotCommands;
+package frc.robot.commands.ShooterPivotCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.AlgaePivot;
+import frc.robot.subsystems.ShooterPivot;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class ManualShooterPivot extends Command {
