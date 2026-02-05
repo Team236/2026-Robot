4f7767181a1afd56cafec0f2c054b522d6ccec78
defiorem742 <mikedefiore742@gmail.com>
2026-02-04 20:29:21 -0500

    a

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -241,6 +241,7 @@ private final Swerve s_Swerve = new Swerve();
     // b.whileTrue(runOuttakeTest);
     upPov.onTrue(Commands.defer(() -> s_Swerve.followPathCommand("hub-center-to-tower-left"), Set.of(s_Swerve)));
     downPov.onTrue(Commands.defer(() -> s_Swerve.followPathCommand("hub-center-to-tower-right"), Set.of(s_Swerve)));
+    rightPov.onTrue(Commands.defer(() -> s_Swerve.followPathCommand("hub-center-to-depot-center"), Set.of(s_Swerve)));
   }
   
     public Command getAutonomousCommand() {
