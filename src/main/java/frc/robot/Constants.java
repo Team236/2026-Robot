0e27b19162f81d52799d1abe5c6fe59c16e4ef02
Spen-ic <spencersek1234@gmail.com>
2026-02-08 10:45:47 -0500

    shooter testing

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -64,7 +64,7 @@ public final class Constants {
   }
 
   public static class ShooterConstants {
-    public static final double MAIN_MOTOR_RPM = 3000; //2200;
+    public static final double MAIN_MOTOR_RPM = 2100; //2200;
     public static final double TOP_MOTOR_RPM = 2000;
 
     public static final double MAIN_MOTOR_SPEED = -0.5;
@@ -132,7 +132,7 @@ public final class Constants {
   public static class PreFeederConstants {
     public static final int DIO_COUNTER = 9;
     public static final double TEST_SPEED = 0.3;
-    public static final int DESIRED_RPM = 2700; //2000;
+    public static final int DESIRED_RPM = 3150; //2000;
     public static final double KV_PF = 0.0975; // feed forward
     public static final double KP_PF = 0.4;
     public static final double KI_PF = 0;
