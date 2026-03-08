7362e6bc48fbfc8364d4f3b0b11fcba2a893e963
Owen Garabedian <Owengarabedian@gmail.com>
2026-03-07 21:40:25 -0500

    New drive inputs and spell checker

--- src/main/java/frc/robot/commands/Targeting/SmartAutoTarget.java
+++ src/main/java/frc/robot/commands/Targeting/SmartAutoTarget.java
@@ -1,10 +1,16 @@
 package frc.robot.commands.Targeting;
-
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.subsystems.Swerve;
 import frc.robot.subsystems.TranslationSubsystem;
 import frc.robot.subsystems.StrafeSubsystem;
 import frc.robot.subsystems.RobotCentricSubsystem;
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
 
 public class SmartAutoTarget extends Command {
   /** Creates a new SmartAutoTarget. */
@@ -16,7 +22,7 @@ public class SmartAutoTarget extends Command {
   private boolean isRedAlliance;
   private double newRotation;
 
-  public SmartAutoTarget(Swerve s_Swerve, TranslationSubsystem translationSup, StrafeSubsystem strafeSup, RobotCentricSubsystem robotCentricSup ) {
+  public SmartAutoTarget(Swerve s_Swerve, DoubleSupplier translationSup, DoubleSupplier strafeSup, BooleanSupplier robotCentricSup) {
     // USING SWERVE FOR TAKING OVER ROTATION
     this.s_Swerve = s_Swerve;
     this.translationSup = translationSup;
@@ -26,7 +32,7 @@ public class SmartAutoTarget extends Command {
     addRequirements(s_Swerve);
   }
 
-  // CURVE FOR DRIVE (CONSTISTANT WITH NORMAL DRIVING)
+  // CURVE FOR DRIVE (CONSISTENT WITH NORMAL DRIVING)
   private double curveDrive(double input) {
     return 0.5 * Math.pow(input, 3) + 0.5 * input;
   }
@@ -35,7 +41,7 @@ public class SmartAutoTarget extends Command {
   public void initialize() {
     var alliance = DriverStation.getAlliance();
 
-    // ALIGENCE SELECTION FROM DRIVER STATION
+    // ALLIANCE SELECTION FROM DRIVER STATION
     if (alliance.isPresent() && alliance.get() == Alliance.Red) {
       isRedAlliance = true;
     } else {
@@ -45,6 +51,9 @@ public class SmartAutoTarget extends Command {
 
   @Override
   public void execute() {
+    double translationVal = MathUtil.applyDeadband(translationSup.getAsDouble(), Constants.stickDeadband);
+    double strafeVal = MathUtil.applyDeadband(strafeSup.getAsDouble(), Constants.stickDeadband);
+
     if (isRedAlliance) {
       if (s_Swerve.inNeutralZone(isRedAlliance)) {
         
