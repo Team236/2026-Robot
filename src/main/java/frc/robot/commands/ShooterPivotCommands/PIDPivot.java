b65c2bd38b8c1bb48661ae4394cfb3172e07cf74
Spen-ic <spencersek1234@gmail.com>
2026-03-24 19:45:49 -0400

    changed pid pivit logic to return false

--- src/main/java/frc/robot/commands/ShooterPivotCommands/PIDPivot.java
+++ src/main/java/frc/robot/commands/ShooterPivotCommands/PIDPivot.java
@@ -44,6 +44,6 @@ public class PIDPivot extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (shooterPivot.isFullyExtended() || shooterPivot.isShooterExtLimit());
+    return false;
   }
 }
